75b316f844ae8aa17f5648cdaf16f8fa54b44e1b
huynhbaokhuyen <huynhbaokhuyen123@gmail.com>
2025-09-24 10:51:00 +0700

    add DataProvider class

--- WebCatalogAuto/src/test/java/testcases/SignInTest.java
+++ WebCatalogAuto/src/test/java/testcases/SignInTest.java
@@ -2,39 +2,26 @@ package testcases;
 
 import base.BaseSetup;
 import listeners.TestListener;
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import pages.SignInPage;
 import utils.LogUtils;
-import utils.readers.ExcelReader;
+import utils.TestDataFile;
+import utils.TestDataProvider;
 
 @Listeners(TestListener.class)
 public class SignInTest extends BaseSetup {
 
-    private WebDriver driver;
-
     public SignInPage signinPage;
 
-    public ExcelReader excel = new ExcelReader();
-
-    @BeforeClass
-    public void setUp() {
-        driver = getDriver();
-    }
-
-    @Test
-    public void signIn() throws Exception {
+    @Test(dataProvider = "testData", dataProviderClass = TestDataProvider.class)
+    @TestDataFile(file = "${loginDataFile}", sheet = "SignIn")
+    public void signIn(String email) throws Exception {
         LogUtils.info("Running login test ...");
 
-        System.out.println(driver);
+        System.out.println(getDriver());
 
-        signinPage = new SignInPage(driver);
-
-        excel.setExcelFile("src/test/resources/data.xlsx", "SignIn");
-
-        String email = excel.getCellData(1, 0);
+        signinPage = new SignInPage(getDriver());
 
         signinPage.signIn(email);
     }
--- WebCatalogAuto/src/test/java/utils/readers/ExcelReader.java
+++ WebCatalogAuto/src/test/java/utils/readers/ExcelReader.java
@@ -1,10 +1,9 @@
 package utils.readers;
 
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -109,5 +108,44 @@ public class ExcelReader {
         }catch(Exception e){
             throw (e);
         }
+    }
+
+    public static Object[][] readExcel(String filePath, String sheetName) {
+        Object[][] data = null;
+
+        try (FileInputStream fis = new FileInputStream(filePath);
+             Workbook workbook = new XSSFWorkbook(fis)) {
+
+            Sheet sheet = workbook.getSheet(sheetName);
+            int rowCount = sheet.getPhysicalNumberOfRows();
+            int colCount = sheet.getRow(0).getPhysicalNumberOfCells();
+
+            data = new Object[rowCount - 1][colCount];
+
+            for (int i = 1; i < rowCount; i++) {
+                Row row = sheet.getRow(i);
+                for (int j = 0; j < colCount; j++) {
+                    Cell cell = row.getCell(j);
+                    data[i - 1][j] = getCellValue(cell);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+
+    private static Object getCellValue(Cell cell) {
+        if (cell == null) return "";
+
+        return switch (cell.getCellType()) {
+            case STRING -> cell.getStringCellValue();
+            case NUMERIC -> DateUtil.isCellDateFormatted(cell) ? cell.getDateCellValue() : cell.getNumericCellValue();
+            case BOOLEAN -> cell.getBooleanCellValue();
+            case FORMULA -> cell.getCellFormula();
+            default -> "";
+        };
     }
 }
